2f85b232f2730c43eb3f5cdb247408ddba449fb6
Robert Pamfile <robert.pamfile@endava.com>
2015-09-28 13:58:47 +0300

    Remove build from remote

--- public/build/app.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../typings/angular2/angular2.d.ts" />
-var angular2_1 = require('angular2/angular2');
-var welcome_component_1 = require('components/welcome.component');
-angular2_1.bootstrap(welcome_component_1.WelcomeComponent);
-//# sourceMappingURL=app.js.map
