b31f1384930e487a2a7f6e6cb4b1329a3425246a
Robert Pamfile <robert.pamfile@endava.com>
2015-09-28 13:57:34 +0300

    Update Angular2 version

--- public/src/components/welcome.component.ts
+++ public/src/components/welcome.component.ts
@@ -1,4 +1,3 @@
-/// <reference path="../../../typings/angular2/angular2.d.ts" />
 
 import {Component, View} from 'angular2/angular2';
 
@@ -12,6 +11,7 @@ import {Component, View} from 'angular2/angular2';
 export class WelcomeComponent {
 
 	name: string;
+	
 	constructor () {
 		this.name = 'Sandeep';
 	}
